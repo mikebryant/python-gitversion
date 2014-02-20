8e318b6ab2e471cd432eb39d58a3c79b5bfebbb1
Mike Bryant <mike@mikebryant.me.uk>
2014-02-20 11:28:52 +0000

    Another classifier.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.5',
